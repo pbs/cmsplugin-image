c2f257d2ee3b3a565eca044b22da807bb41e3e0a
Amalia Gherasim <amalia.gherasim@threepillarglobal.com>
2012-05-14 18:25:06 +0300

    Created the ImageField widget class and registered it to the smartsnippets' widget_pool

---
+++ cmsplugin_image/widgets.py
@@ -0,0 +1,19 @@
+from django.template.loader import render_to_string
+from django.template import RequestContext
+from smartsnippets.widgets_pool import widget_pool
+from smartsnippets.widgets_base import SmartSnippetWidgetBase
+
+
+class ImageField(SmartSnippetWidgetBase):
+    name = 'Image Field'
+    
+    def render(self, request):
+        context_instance = RequestContext(request)
+        return render_to_string('smartsnippets/widgets/imagefield/widget.html',
+            {
+                'field': self.variable,
+                'value_dict': self.formatted_value
+            },
+            context_instance=context_instance)
+        
+widget_pool.register_widget(ImageField)
