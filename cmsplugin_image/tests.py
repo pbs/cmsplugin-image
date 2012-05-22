61d597ff2c5ed4c3443cc79fcb4eb0585f0a9c4c
Amalia Gherasim <amalia.gherasim@threepillarglobal.com>
2012-05-22 15:48:27 +0300

    Removed tearDown calls, because is not necesary to be called in each unit test method.

--- cmsplugin_image/tests.py
+++ cmsplugin_image/tests.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class TestFile(TestCase):
 
-    fixtures = ['mock_data.json' ]
+    fixtures = ['mock_data.json',]
 
     def setUp(self):
         self.client = Client()
@@ -23,12 +23,6 @@ class TestFile(TestCase):
         if not self.logged_in:
             self.logged_in = self.client.login(username='mock_user_%$#!_1234', password='mock')
 
-    def tearDown(self):
-        if self.mock_user:
-            self.logged_in = (not self.client.logout())
-            #self.mock_user.delete()
-        super(TestFile, self).tearDown()
-
     def assertContent(self,  _file, _expected_result):
         if not self.logged_in:
             self.fail("Could not login to the admin interface.")
@@ -40,14 +34,14 @@ class TestFile(TestCase):
     def test_txt(self):
         file = File.objects.get(pk=1)
         self.assertContent(file, '')
-        self.tearDown()
+
 
     def test_png(self):
         file = File.objects.get(pk=2)
         self.assertContent(file, file.file.url)
-        self.tearDown()
+
 
     def test_jpeg(self):
         file = File.objects.get(pk=3)
         self.assertContent(file, file.file.url)
-        self.tearDown()
+
