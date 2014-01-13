32ce27ec227637f3a4d30008d38f87267f4ed465
Laura Feier <laura.feier@3pillarglobal.com>
2014-01-13 16:51:24 +0200

    * new test setup; fixed dependencies; removed broken tests;

--- cmsplugin_image/tests/settings.py
+++ cmsplugin_image/tests/settings.py
@@ -6,7 +6,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'mptt',
+    'cms',
+    'menus',
     'filer',
+    'south',
+    'sekizai',
     'cmsplugin_image',
 ]
 
@@ -22,3 +27,27 @@ DATABASES = {
 }
 
 ROOT_URLCONF = 'cmsplugin_image.tests.urls'
+CMS_TEMPLATES = [('cms_mock_template.html', 'cms_mock_template.html')]
+CMS_MODERATOR = True
+CMS_PERMISSION = True
+TEMPLATE_CONTEXT_PROCESSORS = [
+    "django.contrib.auth.context_processors.auth",
+    'django.contrib.messages.context_processors.messages',
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.request",
+    "django.core.context_processors.media",
+    'django.core.context_processors.csrf',
+    "cms.context_processors.media",
+    "sekizai.context_processors.sekizai",
+    "django.core.context_processors.static",
+]
+MIDDLEWARE_CLASSES = (
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+)
--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from setuptools import setup, find_packages
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                            'README.rst')
 
+dependency_links = [
+    'http://github.com/pbs/django-cms/tarball/support/2.3.x#egg=django-cms-2.3.5pbs',
+    'http://github.com/pbs/django-filer/tarball/master_pbs#egg=django-filer-0.9pbs',
+]
+
 setup(
     name='cmsplugin-image',
     version='0.0.6',
@@ -15,4 +20,12 @@ setup(
     include_package_data=True,
     license='BSD License',
     setup_requires = ['s3sourceuploader',],
+    dependency_links=dependency_links,
+    install_requires=(
+      'PIL',
+      'Django>=1.3,<1.4.8',
+      'django-polymorphic==0.2',
+      'django-cms>=2.3.5pbs, <2.3.6',
+      'django-filer>=0.9pbs, <0.9.1',
+    ),
 )
