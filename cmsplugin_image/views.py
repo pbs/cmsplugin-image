1d159a4995231508124e40b4d51880ffd789502d
Laura Feier <laura.feier@3pillarglobal.com>
2014-10-29 19:11:52 +0200

    LUN-1910: major changes making a base filer file widget that all other filer widgets should extend

--- cmsplugin_image/views.py
+++ cmsplugin_image/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import (
+    HttpResponse, HttpResponseForbidden, HttpResponseBadRequest)
 from django.shortcuts import get_object_or_404
 from filer.models import File, Image
 
@@ -13,13 +14,22 @@ except:
 def get_file(request):
     if not request.is_ajax():
         return HttpResponseForbidden()
-    result = {}
-    file_id = request.GET.get('id');
     try:
-        file = File.objects.get(pk=file_id)
-    except File.DoesNotExist:
-        file = None
+        file_id = int(request.GET.get('id', None))
+    except (TypeError, ValueError):
+        return HttpResponseBadRequest('Filer file missing or not a number')
 
-    result['url'] = file.file.url if file and file.__class__ == Image else ''
+    file_type = (request.GET.get('file_type') or 'image').lower()
+    available_models = {
+        'image': Image,
+        'file': File
+    }
+    model = available_models.get(file_type)
+    if not model:
+        return HttpResponseBadRequest("File type not available.")
 
-    return HttpResponse(json.dumps(result), mimetype="application/json")
+    filer_object = get_object_or_404(model, id=file_id)
+    file_url = filer_object.file.url
+
+    return HttpResponse(
+        json.dumps({'url': file_url}), mimetype="application/json")
--- cmsplugin_image/widgets.py
+++ cmsplugin_image/widgets.py
@@ -4,19 +4,30 @@ from smartsnippets.widgets_pool import widget_pool
 from smartsnippets.widgets_base import SmartSnippetWidgetBase
 
 
-class ImageField(SmartSnippetWidgetBase):
-    name = 'Image Field'
+class FileField(SmartSnippetWidgetBase):
+    name = 'Filer File Field'
 
     def _get_render_options(self):
         return {'field': self.variable,
-                'value_dict': self.formatted_value}
+                'value_dict': self.formatted_value,
+                'file_type': 'file'}
 
     def render(self, request):
-        context_instance = RequestContext(request)
         return render_to_string(
-            'smartsnippets/widgets/imagefield/widget.html',
+            'smartsnippets/widgets/filerfield/widget.html',
             self._get_render_options(),
-            context_instance=context_instance)
+            context_instance=RequestContext(request))
+
+widget_pool.register_widget(FileField)
+
+
+class ImageField(FileField):
+    name = 'Image Field'
+
+    def _get_render_options(self):
+        opts = super(FileField, self)._get_render_options()
+        opts.update({'file_type': 'image'})
+        return opts
 
 widget_pool.register_widget(ImageField)
 
